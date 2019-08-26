6f8c45b4b9168df4a4c0f567598c6cbeb8c944fe
Przemyslaw Bykowski <p.bykowski@sdacademy.pl>
2019-08-26 13:43:12 +0200

    Initial commit

---
+++ src/main/java/pl/bykowski/microservicepatterns/MicroservicePatternsApplication.java
@@ -0,0 +1,13 @@
+package pl.bykowski.microservicepatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicePatternsApplication.class, args);
+    }
+
+}
